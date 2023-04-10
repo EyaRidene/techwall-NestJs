8f171ede5627f1ccef83c69326abc5be5f191e3e
eya ridene <eya.ridene@insat.u-carthage.tn>
2023-04-10 03:59:05 +0100

    typeOrm configuration and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,12 +11,27 @@ import { TodoModule } from './todo/todo.module';
 import { FirstMiddleware } from './middelwares/first/first.middleware';
 import { logger } from './middelwares/first/logger.middelware';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CvModule } from './cv/cv.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
   imports: [
     PremierModule,
     TodoModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CvModule,
   ],
   controllers: [AppController],
   providers: [AppService],
